1a8a8213133352bd8b727b9a15170f90fd2d6a60
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-20 01:45:34 +0300

    globals.css file and utilities

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className={"center"}>
+      <h1 className="h1">CloneIt, Store your file and share easily.</h1>
+    </div>
+  );
+}
