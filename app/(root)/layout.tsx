16ec513a8a87f00c32d34188834f8af66bde95c0
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-21 23:48:29 +0300

    make the main page components file and import in layout.tsx

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+import MobileNavigation from "@/components/MobileNavigation";
+import Header from "@/components/Header";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className={"flex h-screen"}>
+      <Sidebar />
+      <section className={"flex h-full flex-1 flex-col"}>
+        <MobileNavigation /> <Header />
+        <div className={"main-content"}>{children}</div>
+      </section>
+    </main>
+  );
+};
+export default Layout;
