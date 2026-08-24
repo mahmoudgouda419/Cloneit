0353b2cfb25fcca4b6790f19d6e74355338494a7
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-24 08:05:56 +0300

    The functionalty of implementing uploud

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
+import { Toaster } from "@/components/ui/toaster";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const curruntUser = await getCurrentUser();
@@ -14,9 +15,14 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
       <section className={"flex h-full flex-1 flex-col"}>
         <MobileNavigation {...curruntUser} />
-        <Header />
+        <Header
+          userId={curruntUser.$id}
+          accountId={curruntUser.accountId}
+        />{" "}
         <div className={"main-content"}>{children}</div>
       </section>
+
+      <Toaster />
     </main>
   );
 };
