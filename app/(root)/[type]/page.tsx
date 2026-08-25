95bf847080bab4fc55b34a45f38711777b6283e5
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-25 05:54:33 +0300

    file card

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
+import Card from "@/components/Card";
 import { Models } from "node-appwrite";
 
 const Page = async ({ params }: SearchParamProps) => {
@@ -25,9 +26,7 @@ const Page = async ({ params }: SearchParamProps) => {
       {files.total > 0 ? (
         <section className="file-list">
           {files.documents.map((file: Models.Document) => (
-            <h1 key={file.$id} className={"h1"}>
-              {file.name}
-            </h1>
+            <Card key={file.$id} file={file} />
           ))}
         </section>
       ) : (
