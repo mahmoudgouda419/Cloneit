c5f417d0c4f15519e4fab02006e4178e929c6f16
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-27 08:46:54 +0300

    filtering files by format and put them in there correct section

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -3,10 +3,14 @@ import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
 import Card from "@/components/Card";
 import { Models } from "node-appwrite";
+import { getFileTypesParams } from "@/lib/utils";
 
 const Page = async ({ params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
-  const files = await getFiles();
+
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types });
   return (
     <div className={"page-container"}>
       <section className={"w-full"}>
--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -55,7 +55,7 @@ export const uploadFile = async ({
   }
 };
 
-const createQueries = (currentUser: Models.Document) => {
+const createQueries = (currentUser: Models.Document, types: string[]) => {
   const queries = [
     Query.or([
       Query.equal("owner", [currentUser.$id]),
@@ -63,16 +63,18 @@ const createQueries = (currentUser: Models.Document) => {
     ]),
   ];
 
+  if (types.length > 0) queries.push(Query.equal("type", types));
+
   return queries;
 };
 
-export const getFiles = async () => {
+export const getFiles = async ({ types = [] }: GetFilesProps) => {
   const { databases } = await createAdminClient();
   try {
     const currentUser = await getCurrentUser();
     if (!currentUser) throw new Error("User not found :(");
 
-    const queries = createQueries(currentUser);
+    const queries = createQueries(currentUser, types);
     const files = await databases.listDocuments(
       appwriteConfig.databaseId,
       appwriteConfig.filesCollectionId,
