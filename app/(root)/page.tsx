d84ceda7b8a909a72cac3c980b69bde9a3e9ebdc
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-29 22:51:06 +0300

    get used space function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
+
+
+const Dashboard = async Promise.all([
+  const [files, totalSpace] = await Promise.all([
+    getFiles({types: [], limit: 10}),
+    getTotalSpaceUsed(),
+])
+
 export default function Home() {
   return (
     <div className={"flex-center h-screen"}>
