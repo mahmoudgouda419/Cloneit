4927bf6233bc6417fcfb15a29ccdec9a1a20bd2a
Mahmoud Gouda <mahmoudgouda737@gmail.com>
2026-08-30 03:58:39 +0300

    storage chart and file type sections

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { getFiles, getTotalSpaceUsed } from "@/lib/actions/file.actions";
-import { getUsageSummary } from "@/lib/utils";
+import { convertFileSize, getUsageSummary } from "@/lib/utils";
 import { Models } from "node-appwrite";
 import Link from "next/link";
 import { Thumbnail } from "@/components/Thumbnail";
 import FormattedDateTime from "@/components/FormattedDateTime";
 import ActionDropdown from "@/components/ActionDropdown";
+import { Separator } from "@/components/ui/separator";
+import { Chart } from "@/components/Chart";
 
 const Dashboard = async () => {
   // Parallel requests
@@ -18,7 +20,39 @@ const Dashboard = async () => {
 
   return (
     <div className={"dashboard-container"}>
-      <section>{/* Chart Component */}</section>
+      <section>
+        <Chart used={totalSpace.used} />
+        <ul className={"dashboard-summary-list"}>
+          {usageSummary.map((summary) => (
+            <Link
+              href={summary.url}
+              key={summary.title}
+              className={"dashboard-summary-card"}
+            >
+              <div className={"space-y-4"}>
+                <div className={"flex justify-between gap-3"}>
+                  <Image
+                    src={summary.icon}
+                    alt={"uploaded"}
+                    width={100}
+                    height={100}
+                    className={"summary-type-icon"}
+                  />
+                  <h4 className={"summary-type-size"}>
+                    {convertFileSize(summary.size) || 0}
+                  </h4>
+                </div>
+                <h5 className={"summary-type-title"}>{summary.title}</h5>
+                <Separator className="bg-light-400" />
+                <FormattedDateTime
+                  date={summary.latestDate}
+                  className={"text-center"}
+                />
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </section>
       <section className="dashboard-recent-files">
         <h2 className="h3 xl:h2 text-light-100">Recent files uploaded</h2>
         {files.documents.length > 0 ? (
